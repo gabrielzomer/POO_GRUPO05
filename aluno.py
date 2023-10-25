fdf7d2f1975fc20af25fd987a5e2a42f6734dcaf
Gabriel Indriunas Zomer <gabriel.zomer@gmail.com>
2023-10-25 19:29:12 -0300

    atualização aluno

--- aluno.py
+++ aluno.py
@@ -1,12 +1,7 @@
 class Aluno:
 
-    def __init__(self, nome, cpf, email, ano_nascimento):
+    def __init__(self, nome, cpf, email, matricula):
         self._nome = nome
         self._cpf = cpf
         self._email = email
-        self._ano_nascimento = ano_nascimento
-
-    
-    def calcula_idade(self, ano_atual):
-        idade = ano_atual - self._ano_nascimento
-        return idade
+        self._matricula = matricula
