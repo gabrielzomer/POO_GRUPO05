1da45c49443e45256472d9712aa46da1de184909
Gabriel Indriunas Zomer <gabriel.zomer@gmail.com>
2023-10-25 19:29:41 -0300

    correção aluno

--- aluno.py
+++ aluno.py
@@ -2,6 +2,5 @@ class Aluno:
 
     def __init__(self, nome, cpf, email, matricula):
         self._nome = nome
-        self._cpf = cpf
         self._email = email
         self._matricula = matricula
