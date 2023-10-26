d0984413fbba3ea21a35aa378ad7690a8d4f7906
RaphaelGab <aza.gabardo@gmail.com>
2023-10-25 21:16:47 -0300

    Criado o programa que faz o cadastro.

--- aluno.py
+++ aluno.py
@@ -2,3 +2,7 @@ class Aluno:
 
     def __init__(self, matricula):
         self._matricula = matricula
+        self.curso = None #pra come;ar vazio
+
+    def escolher_curso(self, curso):
+        self.curso = curso
